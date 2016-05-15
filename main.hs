3ab3dbc3a7d13eab48c62da0c8d81efa2bcdc292
Delapouite <delapouite@gmail.com>
2016-05-15 16:17:49 +0200

    feat: add show axes related functions

--- main.hs
+++ main.hs
@@ -21,15 +21,21 @@ type Stack = [Stone]
 
 data Cell = Cell Char Int Stack
 instance Show Cell where
-  show (Cell x y _) = x : show y
+  show (Cell x y _) = "."
 
 type Board = [Cell]
 type Col = [Cell]
 type Row = [Cell]
 
 
+size = 5
+-- axes
+xs = ['a'..]
+ys = [1..size]
+
 initBoard :: Int -> Board
-initBoard size = take (size ^ 2) [Cell x y [] | x <- ['a'..], y <- [1..size]]
+initBoard size = take (size ^ 2) [Cell x y [] | x <- xs, y <- ys]
+
 
 toCols :: Board -> [Col]
 toCols b = chunksOf size b
@@ -43,9 +49,22 @@ showBoard = unlines . reverse . map showRow . toRows
 showRow :: Row -> String
 showRow = unwords . map show
 
-size = 5
+
+showBoardWithYAxis :: Board -> String
+showBoardWithYAxis = unlines . reverse . map showRowWithY . toRows
+
+showBoardWithAxes :: Board -> String
+showBoardWithAxes b = showBoardWithYAxis b ++ "  " ++ showXAxis
+
+showRowWithY :: Row -> String
+showRowWithY r = (show y) ++ " " ++ showRow r
+  where
+    (Cell _ y _) = head r
+
+showXAxis :: String
+showXAxis = unwords $ map (\x -> x : []) $ take size xs
 
 
 main = do
   putStrLn "Welcome to Tak.hs"
-  putStrLn $ showBoard $ initBoard size
+  putStrLn $ showBoardWithAxes $ initBoard size
