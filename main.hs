77c4a753e882362e399533363bbc514b4f33b9cd
Delapouite <delapouite@gmail.com>
2016-05-16 16:13:36 +0200

    feat: start place action

--- main.hs
+++ main.hs
@@ -18,20 +18,25 @@ data Player = P1 | P2 deriving (Show)
 -- Flat, Standing or Cap
 data StoneType = F | S | C deriving (Show)
 
-type Stone = (Player, StoneType)
+-- P1 in UpperCase, P2 in LowerCase
+data Stone = Stone Player StoneType
+instance Show Stone where
+  show (Stone P1 F) = "F"
+  show (Stone P2 F) = "f"
 
 -- bottom to top
 type Stack = [Stone]
 
 data Cell = Cell Char Int Stack
 instance Show Cell where
-  show (Cell x y _) = "."
+  show (Cell _ _ s) = showStack s
 
 type Board = [Cell]
 type Col = [Cell]
 type Row = [Cell]
 
 type Verb = String
+type Coord = String
 data Action = Action Verb [String]
 
 -- axis
@@ -40,8 +45,26 @@ xs = ['a'..]
 initBoard :: Int -> Board
 initBoard size = take (size ^ 2) [Cell x y [] | x <- xs, y <- [1..size]]
 
+stoneCount :: Int -> Int
+stoneCount 3 = 10
+stoneCount 4 = 15
+stoneCount 5 = 21
+stoneCount 6 = 30
+stoneCount 7 = 40
+stoneCount 8 = 50
+
+capCount :: Int -> Int
+capCount 8 = 2
+capCount 4 = 0
+capCount 3 = 0
+capCount _ = 1
+
 getSize :: Board -> Int
 getSize = truncate . sqrt . fromIntegral . length
+
+-- TODO
+isValidCoord :: Board -> Coord -> Bool
+isValidCoord b c = True
 
 toCols :: Board -> [Col]
 toCols b = chunksOf (getSize b) b
@@ -82,12 +105,24 @@ showRowWithY r = (show y) ++ " " ++ showCells r
 showXAxis :: Board -> String
 showXAxis b = unwords $ map (\x -> x : []) $ take (getSize b) xs
 
+showStack :: Stack -> String
+showStack [] = "."
+showStack s = show $ last s
+
 -- actions
 
 parseAction :: String -> Action
 parseAction s = Action verb args
   where
     (verb:args) = words s
+
+placeStone :: Board -> Coord -> Board
+placeStone b coord = map (stackStone coord) b
+
+stackStone :: Coord -> Cell -> Cell
+stackStone (x:y) c@(Cell cx cy _) = if x == cx && (read y :: Int) == cy
+  then Cell cx cy [Stone P1 F]
+  else c
 
 -- IO
 
@@ -97,10 +132,17 @@ handleShow b coord = do
     True  -> putStrLn $ showRowByY b $ read coord
     False -> putStrLn $ showColByX b $ head coord
 
+handlePlace :: Board -> Coord -> IO ()
+handlePlace b coord = do
+  case isValidCoord b coord of
+    True -> putStrLn $ showBoardWithAxes $ placeStone b coord
+    False -> putStrLn "Wrong coordinates xy"
+
 handleAction :: Board -> Action -> IO ()
 handleAction b a = do
   case a of
     (Action "show" (coord:cs)) -> handleShow b coord
+    (Action "place" (coord:cs)) -> handlePlace b coord
     _ -> putStrLn "Unknown action"
 
 loop :: Board -> Player -> IO ()
@@ -119,7 +161,7 @@ prompt q = do
 
 main = do
   putStrLn "Welcome to Tak.hs"
-  size <- prompt "Size of the board?"
+  size <- prompt "Size of the board? [3..8]"
   let b = initBoard (read size :: Int)
   putStrLn $ showBoardWithAxes b
   loop b P1
