616fdcff2c64ae8ac18959d38d1b703db9f183c5
Delapouite <delapouite@gmail.com>
2016-05-16 13:01:41 +0200

    feat: add loop and basic action parsing

--- main.hs
+++ main.hs
@@ -28,13 +28,14 @@ type Board = [Cell]
 type Col = [Cell]
 type Row = [Cell]
 
+type Verb = String
+data Action = Action Verb [String]
 
 -- axis
 xs = ['a'..]
 
 initBoard :: Int -> Board
 initBoard size = take (size ^ 2) [Cell x y [] | x <- xs, y <- [1..size]]
-
 
 getSize :: Board -> Int
 getSize = truncate . sqrt . fromIntegral . length
@@ -45,12 +46,13 @@ toCols b = chunksOf (getSize b) b
 toRows :: Board -> [Row]
 toRows = transpose . toCols
 
+-- display
+
 showBoard :: Board -> String
 showBoard = unlines . reverse . map showRow . toRows
 
 showRow :: Row -> String
 showRow = unwords . map show
-
 
 showBoardWithYAxis :: Board -> String
 showBoardWithYAxis = unlines . reverse . map showRowWithY . toRows
@@ -66,6 +68,28 @@ showRowWithY r = (show y) ++ " " ++ showRow r
 showXAxis :: Board -> String
 showXAxis b = unwords $ map (\x -> x : []) $ take (getSize b) xs
 
+-- actions
+
+parseAction :: String -> Action
+parseAction s = Action verb args
+  where
+    (verb:args) = words s
+
+-- IO
+
+handleAction :: Action -> IO ()
+handleAction a = do
+  case a of
+    (Action "show" (x:xs)) -> putStrLn $ "Let's show " ++ x
+    _ -> putStrLn "Unknown action"
+
+loop :: Board -> Player -> IO ()
+loop b p = do
+  action <- prompt $ show p ++ ">"
+  handleAction $ parseAction action
+  loop b p
+
+prompt :: String -> IO String
 prompt q = do
   putStr $ q ++ " "
   -- http://stackoverflow.com/questions/21853343/haskell-putstr-vs-putstrln-and-instruction-order
@@ -76,6 +100,6 @@ prompt q = do
 main = do
   putStrLn "Welcome to Tak.hs"
   size <- prompt "Size of the board?"
-  putStrLn $ showBoardWithAxes $ initBoard (read size :: Int)
-  action <- prompt "P1>"
-  putStrLn action
+  let b = initBoard (read size :: Int)
+  putStrLn $ showBoardWithAxes b
+  loop b P1
