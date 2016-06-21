4672838a9c86d1c5fa740127c0e0ffb6b2ee6b8c
Delapouite <delapouite@gmail.com>
2016-06-21 16:15:55 +0200

    test: add xToInt and toXorY

--- test/ConversionSpec.hs
+++ test/ConversionSpec.hs
@@ -5,10 +5,21 @@ import Test.HUnit
 import Tak
 import Conversion
 
-testGetSize = TestCase $ assertEqual "Should return side length of the Board" exp act
+testsXtoInt = TestList
+  [ TestCase $ xToInt 'c' @?= 2 ]
+
+testsToXorY = TestList
+  [ TestCase $ toXorY "c" @?= Left 'c'
+  , TestCase $ toXorY "4" @?= Right 4
+  ]
+
+testsGetSize = TestCase $ assertEqual "Should return side length of the Board" exp act
   where
     exp = 5
     act = getSize $ initBoard exp
 
 testsConversion = TestList
-  [ testGetSize ]
+  [ testsGetSize
+  , testsXtoInt
+  , testsToXorY
+  ]
