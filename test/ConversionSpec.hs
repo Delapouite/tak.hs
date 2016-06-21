7c5df2ea765cffc66db7baa70c6cac215aeb3f00
Delapouite <delapouite@gmail.com>
2016-06-21 16:32:10 +0200

    test: add getHeight and getTallestHeight

--- test/ConversionSpec.hs
+++ test/ConversionSpec.hs
@@ -5,6 +5,12 @@ import Test.HUnit
 import Tak
 import Conversion
 
+mockStack1 = [Stone P1 F, Stone P2 F, Stone P1 S]
+mockStack2 = [Stone P2 F, Stone P1 C]
+
+mockCell1 = Cell ('b', 3) mockStack1
+mockCell2 = Cell ('c', 4) mockStack2
+
 testsXtoInt = TestList
   [ TestCase $ xToInt 'c' @?= 2 ]
 
@@ -18,8 +24,17 @@ testsGetSize = TestCase $ assertEqual "Should return side length of the Board" e
     exp = 5
     act = getSize $ initBoard exp
 
+testsGetHeight = TestList
+  [ TestCase $ getHeight mockCell1 @?= 3
+  , TestCase $ getHeight mockCell2 @?= 2
+  ]
+
+testsGetTallestHeight = TestList
+  [ TestCase $ getTallestHeight [mockCell1, mockCell2] @?= 3 ]
+
 testsConversion = TestList
   [ testsGetSize
   , testsXtoInt
   , testsToXorY
+  , testsGetHeight
   ]
