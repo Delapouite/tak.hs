a64735de76966585a785da434cddf1877b5798fb
Delapouite <delapouite@gmail.com>
2016-06-23 10:32:49 +0200

    test: add isBoardFull

--- test/ValidationSpec.hs
+++ test/ValidationSpec.hs
@@ -5,6 +5,27 @@ import Test.HUnit
 import Tak
 import Validation
 
+testsIsBoardFull = TestList
+  [ TestCase $ False @=? isBoardFull
+     [ Cell ('a', 1) []
+     , Cell ('a', 2) []
+     , Cell ('b', 1) []
+     , Cell ('b', 2) []
+     ]
+  , TestCase $ False @=? isBoardFull
+     [ Cell ('a', 1) [Stone P1 F]
+     , Cell ('a', 2) []
+     , Cell ('b', 1) [Stone P2 C]
+     , Cell ('b', 2) []
+     ]
+  , TestCase $ True @=? isBoardFull
+     [ Cell ('a', 1) [Stone P1 F]
+     , Cell ('a', 2) [Stone P1 F]
+     , Cell ('b', 1) [Stone P2 C]
+     , Cell ('b', 2) [Stone P2 S]
+     ]
+  ]
+
 testsIsToppable = TestList
   [ TestCase $ isToppable (Cell ('a', 1) []) @?= True
   , TestCase $ isToppable (Cell ('a', 1) [Stone P1 F]) @?= True
@@ -20,6 +41,7 @@ testsHasCap = TestList
   ]
 
 testsValidation = TestList
-  [ testsIsToppable
+  [ testsIsBoardFull
+  , testsIsToppable
   , testsHasCap
   ]
