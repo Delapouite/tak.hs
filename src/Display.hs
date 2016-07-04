e3083a67e63e7a00771342ec9d7f88325195c57c
Delapouite <delapouite@gmail.com>
2016-07-04 14:23:57 +0200

    docs: add more comments to Display

--- src/Display.hs
+++ src/Display.hs
@@ -14,37 +14,25 @@ toColor :: Player -> String -> String
 toColor P1 = show . blue . text
 toColor P2 = show . yellow . text
 
+-- order from small to big components
+
+-- in prompt or decks
 showPlayer :: Player -> ROptions Display
 showPlayer p = do
   colored <- asks optColors
   return $ if colored then toColor p $ show p else show p
 
+-- capital letters for P1
+showStoneType :: StoneType -> Player -> Display
+showStoneType t P1 = show t
+showStoneType t P2 = map toLower $ show t
+
+-- stoneType
 showStone :: Stone -> ROptions Display
 showStone (Stone p t) = do
   colored <- asks optColors
   let disp = showStoneType t p
   return $ if colored then toColor p disp else disp
-
-showStoneType :: StoneType -> Player -> Display
-showStoneType t P1 = show t
-showStoneType t P2 = map toLower $ show t
-
-showBoard :: Board -> ROptions Display
-showBoard b = do
-  rows <- mapM showCells $ toRows b
-  return $ unlines . reverse $ rows
-
-showCell :: Cell -> ROptions Display
-showCell (Cell _ zs) = showStack zs
-
-showCells :: [Cell] -> ROptions Display
-showCells cs = do
-  cells <- mapM showCell cs
-  return $ unwords cells
-
-showStack :: Stack -> ROptions Display
-showStack [] = return "."
-showStack zs = showStone $ last zs
 
 showStoneAtLevel :: Int -> Cell -> ROptions Display
 showStoneAtLevel lvl (Cell _ zs) =
@@ -52,26 +40,56 @@ showStoneAtLevel lvl (Cell _ zs) =
   then showStone (zs !! lvl)
   else return " "
 
+-- top view
+showStack :: Stack -> ROptions Display
+showStack [] = return "."
+showStack zs = showStone $ last zs
+
 showStackLevel :: [Cell] -> Int -> ROptions Display
 showStackLevel cs lvl = do
   stones <- mapM (showStoneAtLevel lvl) cs
   return $ unwords stones
 
+-- side view
 showStacks :: [Cell] -> ROptions Display
 showStacks cs = do
   let levels = reverse [0..getMaxHeight cs - 1]
   stackLevels <- mapM (showStackLevel cs) levels
   return $ unlines stackLevels
 
+-- top view
+showCell :: Cell -> ROptions Display
+showCell (Cell _ zs) = showStack zs
+
+-- col or row
+showCells :: [Cell] -> ROptions Display
+showCells cs = do
+  cells <- mapM showCell cs
+  return $ unwords cells
+
+-- side view
 showCol :: Board -> X -> ROptions Display
 showCol b x = do
   col <- showStacks . reverse $ getCol b x
   return $ col ++ showYAxis b
 
+-- side view
 showRow :: Board -> Y -> ROptions Display
 showRow b y = do
   row <- showStacks $ getRow b y
   return $ row ++ showXAxis b
+
+-- top view
+showRowWithY :: Row -> ROptions Display
+showRowWithY r = do
+  let (Cell (_, y) _) = head r
+  cells <- showCells r
+  return $ show y ++ " " ++ cells
+
+showBoard :: Board -> ROptions Display
+showBoard b = do
+  rows <- mapM showCells $ toRows b
+  return $ unlines . reverse $ rows
 
 showBoardWithYAxis :: Board -> ROptions Display
 showBoardWithYAxis b = do
@@ -83,16 +101,11 @@ showBoardWithAxes b = do
   withYAxis <- showBoardWithYAxis b
   return $ "\n" ++ withYAxis ++ "  " ++ showXAxis b
 
-showRowWithY :: Row -> ROptions Display
-showRowWithY r = do
-  let (Cell (_, y) _) = head r
-  cells <- showCells r
-  return $ show y ++ " " ++ cells
-
--- horizontally
+-- horizontally 5 4 3 2 1
 showYAxis :: Board -> Display
 showYAxis b = unwords $ map show $ reverse $ take (getSize b) [1..]
 
+-- a b c d e
 showXAxis :: Board -> Display
 showXAxis b = unwords $ map (: []) $ take (getSize b) xs
 
